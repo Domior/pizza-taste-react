5801abf820d9b3dce2be0f933120a3a33f4ecbd0
Andrew Snisar <andrew.snisar@gmail.com>
2022-07-05 23:18:53 +0300

    render categories with selection on click

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
 const Categories = () => {
+  const categories = ['Все', 'Мясные', 'Вегетарианские', 'Острые', 'С грибами'];
+
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories?.map((category, i) => (
+          <li
+            key={i}
+            className={activeIndex === i ? 'active' : ''}
+            onClick={() => setActiveIndex(i)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
