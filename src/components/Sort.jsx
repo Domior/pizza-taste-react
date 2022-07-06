0e4e27c97aca01b2c72963c49626e24d18c180ac
Andrew Snisar <andrew.snisar@gmail.com>
2022-07-06 19:38:37 +0300

    created sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -3,20 +3,40 @@ import React from 'react';
 import { ReactComponent as ArrowIcon } from '../assets/img/arrow-top.svg';
 
 const Sort = () => {
+  const sortList = ['популярности', 'цене', 'алфавиту'];
+
+  const [open, setOpen] = React.useState(false);
+  const [selectedSort, setSelectedSort] = React.useState(sortList[0]);
+
+  const handleClick = () => setOpen(!open);
+
+  const handleClickSort = title => {
+    setSelectedSort(title);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
         <ArrowIcon />
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={handleClick}>{selectedSort}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {sortList?.map(title => (
+              <li
+                key={title}
+                className={selectedSort === title ? 'active' : ''}
+                onClick={() => handleClickSort(title)}
+              >
+                {title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
